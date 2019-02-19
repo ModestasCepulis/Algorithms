311cb0c7d1e0435e94540d22656c70cba676fc6c
Modestas Cepulis <modestas.cepulis@students.ittralee.ie>
2019-02-19 10:38:11 +0000

    Lab 7 finished

--- src/MyArrays.java
+++ src/MyArrays.java
@@ -53,7 +53,7 @@ public class MyArrays {
         }
     }
 
-    public static void insertionSort(int [] a)
+    /*public static void insertionSort(int [] a)
     {
         for(int i; a.length<1; i++)
         {
@@ -67,5 +67,64 @@ public class MyArrays {
             }
             a[j+val] = val;
         }
+    }*/
+
+    public static void merge(int []first, int []second, int [] a)
+    {
+        int iFirst = 0;
+        int iSecond = 0;
+        int j = 0;
+
+        while(iFirst < first.length && iSecond < second.length)
+        {
+            if(first[iFirst] < second[iSecond])
+            {
+                a[j]=first[iFirst];
+                iFirst++;
+            }
+            else
+            {
+                a[j]=second[iSecond];
+                iSecond++;
+            }
+            j++;
+        }
+
+        while(iFirst<first.length)
+        {
+            a[j]=first[iFirst];
+            iFirst++;
+            j++;
+        }
+
+        while (iSecond<second.length)
+        {
+            a[j]=second[iSecond];
+            iSecond++;
+            j++;
+        }
+
+
     }
+
+    public static void mergeSort(int[] a)
+    {
+        if (a.length <= 1) { return; }
+        int[] first = new int[a.length / 2];
+        int[] second = new int[a.length - first.length];
+        // Copy the first half of a into first, the second half into second
+        for (int i = 0; i < first.length; i++)
+        {
+            first[i] = a[i];
+        }
+        for (int i = 0; i < second.length; i++)
+        {
+            second[i] = a[first.length + i];
+        }
+
+        mergeSort(first);
+        mergeSort(second);
+        merge(first, second, a);
+    }
+
 }
--- src/TestMyArrays.java
+++ src/TestMyArrays.java
@@ -1,6 +1,9 @@
+import java.util.Random;
+
 public class TestMyArrays {
     public static void main(String[] args) {
-        
+
+        /*
         int [] values = new int[100];
         values[0] = 1;
         values[1] = 4;
@@ -15,6 +18,66 @@ public class TestMyArrays {
        
         for (int i=0; i < currentSize; i++)
             System.out.println(values[i]);
+        */
 
+        /*
+        int [] values = new int[5];
+        values[0] = 25;
+        values[1] = 3;
+        values[2] = 17;
+        values[3] = 2;
+        values[4] = 1;
+
+        MyArrays.mergeSort(values);
+
+        for(int i=0; i<values.length; i++)
+        {
+            System.out.println(values[i] + " ,");
         }
+        */
+
+        //Random created array
+        /*
+        Random random = new Random();
+        final int SIZE = 10000;
+        int[] a = new int[SIZE];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = random.nextInt(Integer.MAX_VALUE);
+        }
+        */
+
+        //Already sorted array
+        final int SIZE = 10000;
+        int[] a = new int[SIZE];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = i;
+        }
+
+        a[0] = 1;
+        a[1] = 5;
+        a[2] = 12;
+        a[3] = 52;
+        a[4] = 2;
+        a[5] = 2950;
+        a[6] = 125125;
+        a[7] = 123;
+        a[8] = 24902;
+        a[9] = 11111;
+
+
+        //MyArrays.mergeSort(a);
+        MyArrays.insert(a, 20, 2,5);
+
+        long sTime = System.currentTimeMillis();
+
+        for(int i=0; i<a.length; i++)
+        {
+            System.out.println(a[i] + " ,");
+        }
+
+        long fTime = System.currentTimeMillis();
+
+        System.out.println(fTime - sTime + " ms");
+
+    }
 }
